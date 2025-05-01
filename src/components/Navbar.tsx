f5b15c1f1f7e2f5de555dac10bcf7c82cbef63a9
satpal87 <44978057+satpal87@users.noreply.github.com>
2025-05-01 13:46:54 +0200

    Change in color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex items-center justify-between">
           <Link to="/" className="flex items-center">
-            <span className="text-2xl font-serif font-bold text-theme-blue">Adv. <span className="text-theme-red">Jasvinder</span> Singh Ply</span>
+            <span className="text-2xl font-serif font-bold text-theme-blue">Adv. <span className="text-theme-white">Jasvinder</span> Singh Ply</span>
           </Link>
 
           {/* Desktop Menu */}
